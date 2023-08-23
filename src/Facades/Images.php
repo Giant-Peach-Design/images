11068877ee011ce6479bca9ecbba9310d8e02f99
Tom Gurney <=>
2023-08-23 09:24:31 +0100

    wip: facade

--- src/Facades/Images.php
+++ src/Facades/Images.php
@@ -2,10 +2,12 @@
 
 namespace Giantpeach\Schnapps\Images\Facades;
 
+use Giantpeach\Schnapps\Images\Images as ImagesInstance;
+
 class Images
 {
   public static function __callStatic($name, $arguments)
   {
-    return call_user_func_array([Images::getInstance(), $name], $arguments);
+    return call_user_func_array([ImagesInstance::getInstance(), $name], $arguments);
   }
 }
